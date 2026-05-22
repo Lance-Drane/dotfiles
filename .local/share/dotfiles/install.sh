40fc6c03432466fe463810aa44a48fb42ac7918b
Lance-Drane <Lance-Drane@users.noreply.github.com>
2026-05-21 22:11:49 -0400

    use printf instead of echo for portability in install script
    Signed-off-by: Lance-Drane <Lance-Drane@users.noreply.github.com>

--- .local/share/dotfiles/install.sh
+++ .local/share/dotfiles/install.sh
@@ -14,14 +14,14 @@ dotfiles() {
 }
 checkout_result=$(dotfiles checkout 2>&1)
 if [ $? -eq 0 ]; then
-	echo "Added dotfiles to home directory"
+	printf 'Added dotfiles to home directory\n'
 else
-	BACKUPS=$(echo "$checkout_result" | grep -E "^\s+[\.|README|LICENSE]" | awk '{print $1}')
-	echo "Backing up these dotfiles in $DOTFILES_BACKUP_DIR:"
-	echo "$BACKUPS"
+	BACKUPS=$(printf '%s' "$checkout_result" | grep -E '^\s+[\.|README|LICENSE]' | awk '{print $1}')
+	printf 'Backing up these dotfiles in %s:\n' "$DOTFILES_BACKUP_DIR"
+	printf '%s\n' "$BACKUPS"
 	# move by file, not by entire directory
 	for line in $BACKUPS; do mkdir -p "$(dirname "${DOTFILES_BACKUP_DIR}/${line}")"; done
-	echo "$BACKUPS" | xargs -I{} mv "$HOME"/{} "${DOTFILES_BACKUP_DIR}"/{}
+	printf '%s' "$BACKUPS" | xargs -I{} mv "$HOME"/{} "${DOTFILES_BACKUP_DIR}"/{}
 	dotfiles checkout
 fi
 dotfiles config status.showUntrackedFiles no
