c5a9f488eb1d3cb07be442661e8c459710971820
Lance-Drane <Lance-Drane@users.noreply.github.com>
2026-08-25 05:46:51 -0400

    add an oxwm, rofi, picom configuration
    Signed-off-by: Lance-Drane <Lance-Drane@users.noreply.github.com>

---
+++ .config/oxwm/themes/tokyonight_night.lua
@@ -0,0 +1,17 @@
+return {
+	-- UI colors
+	bg = "#1a1b26",
+	fg = "#c0caf5",
+	critical = "#f7768e",
+	warning = "#e0af68",
+	success = "#9ece6a",
+	info = "#7dcfff",
+	focus = "#bb9af7",
+	-- block config
+	music = "#c0caf5",
+	ram = "#7aa2f7",
+	cpu = "#f7768e",
+	volume = "#7dcfff",
+	datetime = "#9ece6a",
+	battery = "#e0af68",
+}
