67f12d58d3942bbf6d9166194b8dff3c8ae37fc6
Lance-Drane <Lance-Drane@users.noreply.github.com>
2026-08-28 01:35:41 -0400

    nvim - nicer half-scroll behavior
    Signed-off-by: Lance-Drane <Lance-Drane@users.noreply.github.com>

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- keep cursor centered while scrolling, sometimes causes jump when scrolling down from first line
+local function stopflicker(keys)
+	keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+	return function()
+		local old = vim.o.lazyredraw
+		vim.o.lazyredraw = true
+		vim.api.nvim_feedkeys(keys, "nx", false)
+		vim.o.lazyredraw = old
+	end
+end
+
+vim.keymap.set("n", "<C-d>", stopflicker("<C-d>zz"), { desc = "Scroll down half page and center cursor" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half page and center cursor" })
